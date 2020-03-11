9edd7804526269bc6dd55715abb5cb336cb8c863
DanBea2019 <53918887+DanBea2019@users.noreply.github.com>
2020-03-11 16:16:46 -0600

    Update Carta.java

--- src/Carta.java
+++ src/Carta.java
@@ -9,8 +9,8 @@ import java.util.Map.Entry;
  */
 
 /**
- *
- * @author marianam
+ * @author Daniela Batz 19214
+ * @author Mariana Morales 17235
  */
 public class Carta {
     private String nombre;
